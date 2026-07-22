69c49503aa06171d501e4b2e1ce27f867804defb
ezvaar <tshepiso.khunou@gmail.com>
2026-07-22 16:48:16 +0200

    Create MainActivity.java

---
+++ app/MainActivity.java
@@ -0,0 +1,29 @@
+package com.h.a;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Request required runtime permissions for Android 8+ and 13+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissions(new String[]{
+                    Manifest.permission.NEARBY_WIFI_DEVICES,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 101);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 101);
+        } else {
+            Toast.makeText(this, "Permissions granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
